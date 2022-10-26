70ce7a0358abb6fd41845dd2fbd5601ff8454b8b
dtaleme <geek.cubed@outlook.com>
2022-10-26 23:58:43 +0300

    initial commit

---
+++ modules/chausov_n_bit_array/include/bit_array.h
@@ -0,0 +1,28 @@
+// Copyright 2022 Chausov Nikita
+
+#ifndef MODULES_CHAUSOV_N_BIT_ARRAY_INCLUDE_BIT_ARRAY_H_
+#define MODULES_CHAUSOV_N_BIT_ARRAY_INCLUDE_BIT_ARRAY_H_
+
+#include <string>
+
+class BitArray {
+ private:
+  char* arr;
+  unsigned int bitSize;
+  unsigned int size;
+
+ public:
+  BitArray();
+  explicit BitArray(const unsigned int _bitSize);
+  BitArray(const BitArray& arrCopy);
+
+  int GetSizeBits();
+  void SetBit(unsigned int bitPos, bool val);
+  bool GetBit(unsigned int bitPos);
+
+  bool IsEmpty();
+  void DeleteArray();
+  void AllocateArray(int _bitSize);
+};
+
+#endif  // MODULES_CHAUSOV_N_BIT_ARRAY_INCLUDE_BIT_ARRAY_H_
